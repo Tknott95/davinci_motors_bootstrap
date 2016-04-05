7b636e8daed59ffac0184d2a4e5109438a0a91fc
Trevor Knott <Tknott95@hotmail.com>
2016-04-05 16:06:10 -0600

    Created cars scaffold with bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  config.generators do |g|
+    g.controller_specs false
+    g.helper_specs false
+    g.routung_specs false
+    g.view_specs false
+  end
 end
